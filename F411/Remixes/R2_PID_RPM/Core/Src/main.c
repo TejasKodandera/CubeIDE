119b96c8852e3f9dc56bde7c637168bd03232ac6
Tejas Kodandera <tejaskodandera@gmail.com>
2024-06-15 22:35:17 +0530

    PID RPM code (untested)

--- F411/Remixes/R2_PID_RPM/Core/Src/main.c
+++ F411/Remixes/R2_PID_RPM/Core/Src/main.c
@@ -31,11 +31,16 @@
 /* USER CODE BEGIN PTD */
 typedef struct {
 	GPIO_TypeDef *PORT;
-	uint16_t PIN;
+	uint16_t DIR_PIN;
 	TIM_HandleTypeDef TIMER;
 	uint32_t CHANNEL;
+	uint16_t A_PIN;
+	uint16_t B_PIN;
 	uint8_t DIR;
 	uint16_t PWM;
+	uint16_t CUR_RPM;
+	uint16_t TAR_RPM;
+	uint32_t LAST_TICK;
 } MOT_TypeDef;
 
 /* USER CODE END PTD */
@@ -43,11 +48,17 @@ typedef struct {
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define BUFFER_LENGTH 64
-#define SOL_INTERVAL 1000
+#define SOL_INTERVAL 10000
 
 #define NO_OF_MOTORS 2
-#define LEFT_SHIFT 8
+#define LEFT_SHIFT 1
 
+#define KP 1
+#define KD 1
+#define KI 1
+
+#define MAX_PWM 65535
+#define MIN_PWM 0
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -64,6 +75,16 @@ char buffer[BUFFER_LENGTH];
 
 uint8_t errorFlag = 0;
 
+uint16_t ms = 0;
+uint16_t output = 0;
+
+uint32_t currentTime;
+
+int16_t integral = 0;
+int16_t previousError = 0;
+
+MOT_TypeDef motor[NO_OF_MOTORS];
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -77,6 +98,26 @@ static void MX_TIM4_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void updateTime() {
+	currentTime = ms * 1000 + TIM4->CNT;
+}
+
+void updateEncoder(uint8_t motIndex) {
+	updateTime();
+
+	motor[motIndex].CUR_RPM = 60000000
+			/ (currentTime - motor[motIndex].LAST_TICK);
+}
+
+void PIDController(float targetRPM, float currentRPM) {
+	int16_t error = targetRPM - currentRPM;
+	integral += error;
+	int16_t derivative = error - previousError;
+	output = (KP * error) + (KI * integral) + (KD * derivative);
+
+	previousError = error;
+}
+
 int isCleared(char *buffer) {
 	for (uint8_t i = 0; i < BUFFER_LENGTH; i++) {
 		if (buffer[i] != '\0') {
@@ -93,26 +134,31 @@ int isCleared(char *buffer) {
  * @retval int
  */
 int main(void) {
-
 	/* USER CODE BEGIN 1 */
 	uint32_t lastSOL = 0;
 
 	char message[BUFFER_LENGTH + 20];
 
-	MOT_TypeDef motor[NO_OF_MOTORS];
 	motor[0].PORT = GPIOB;
-	motor[0].PIN = GPIO_PIN_12;
+	motor[0].DIR_PIN = GPIO_PIN_12;
 	motor[0].TIMER = htim1;
 	motor[0].CHANNEL = TIM_CHANNEL_1;
-	motor[0].DIR = 0;
-	motor[0].PWM = 0;
+	motor[0].A_PIN = GPIO_PIN_6;
+	motor[0].B_PIN = GPIO_PIN_7;
 
 	motor[1].PORT = GPIOB;
-	motor[1].PIN = GPIO_PIN_14;
+	motor[1].DIR_PIN = GPIO_PIN_14;
 	motor[1].TIMER = htim1;
 	motor[1].CHANNEL = TIM_CHANNEL_3;
-	motor[1].DIR = 0;
-	motor[1].PWM = 0;
+	motor[1].A_PIN = GPIO_PIN_8;
+	motor[1].B_PIN = GPIO_PIN_9;
+
+	for (uint8_t i = 0; i < NO_OF_MOTORS; i++) {
+		motor[i].DIR = 0;
+		motor[i].PWM = 0;
+		motor[i].TAR_RPM = 0;
+		motor[i].CUR_RPM = 0;
+	}
 	/* USER CODE END 1 */
 
 	/* MCU Configuration--------------------------------------------------------*/
@@ -133,8 +179,8 @@ int main(void) {
 
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
-	MX_USB_DEVICE_Init();
 	MX_TIM1_Init();
+	MX_USB_DEVICE_Init();
 	MX_TIM4_Init();
 	/* USER CODE BEGIN 2 */
 	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
@@ -147,7 +193,7 @@ int main(void) {
 			errorFlag = 1;
 		}
 
-		HAL_GPIO_WritePin(motor[i].PORT, motor[i].PIN, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(motor[i].PORT, motor[i].DIR_PIN, GPIO_PIN_RESET);
 		__HAL_TIM_SET_COMPARE(&(motor[i].TIMER), motor[i].CHANNEL, 0);
 
 		CDC_Transmit_FS((uint8_t*) message, strlen(message));
@@ -172,13 +218,14 @@ int main(void) {
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
 	while (1) {
-		uint32_t current_time = HAL_GetTick();
+		updateTime();
 
 		if (!isCleared(buffer)) {
 			char receivedStr[BUFFER_LENGTH];
 
 			sprintf(message, "String Received: %s\n\r", buffer);
 			CDC_Transmit_FS((uint8_t*) message, strlen(message));
+			HAL_Delay(1);
 
 			sprintf(receivedStr, "%s", buffer);
 			uint8_t receivedStrlen = strlen(receivedStr);
@@ -189,12 +236,12 @@ int main(void) {
 
 				for (uint8_t i = 0; i < NO_OF_MOTORS; i++) {
 					motor[i].DIR = 0;
-					motor[i].PWM = 0;
+					motor[i].TAR_RPM = 0;
 				}
 
 				for (int8_t i = receivedStrlen - 2; i >= 0; i--) {
 					if (receivedStr[i] >= '0' && receivedStr[i] <= '9') {
-						motor[arrIndex].PWM += (receivedStr[i] - '0')
+						motor[arrIndex].TAR_RPM += (receivedStr[i] - '0')
 								* multiplier;
 						multiplier *= 10;
 					} else if (receivedStr[i] == '-') {
@@ -204,24 +251,42 @@ int main(void) {
 						multiplier = 1;
 					}
 				}
-			}
 
-			for (uint8_t i = 0; i < NO_OF_MOTORS; i++) {
-				HAL_GPIO_WritePin(motor[i].PORT, motor[i].PIN, motor[i].DIR);
-				__HAL_TIM_SET_COMPARE(&(motor[i].TIMER), motor[i].CHANNEL,
-						motor[i].PWM << LEFT_SHIFT);
-
-				sprintf(message, "Dir%d: %d PWM%d: %d\n\r", i, motor[i].DIR, i,
-						motor[i].PWM);
-				CDC_Transmit_FS((uint8_t*) message, strlen(message));
-				HAL_Delay(1);	//Doesn't work without this line
+				for (uint8_t i = 0; i < NO_OF_MOTORS; i++) {
+					HAL_GPIO_WritePin(motor[0].PORT, motor[0].DIR_PIN,
+							motor[0].DIR);
+				}
 			}
 
 			memset(buffer, '\0', BUFFER_LENGTH);
 
-		} else if (current_time - lastSOL >= SOL_INTERVAL) {
+		} else if (currentTime - lastSOL >= SOL_INTERVAL) {
 			CDC_Transmit_FS((uint8_t*) "SOL\n\r", strlen("SOL\n\r"));
-			lastSOL = current_time;
+			lastSOL = currentTime;
+		}
+
+		for (uint8_t i = 0; i < NO_OF_MOTORS; i++) {
+			PIDController(motor[i].TAR_RPM, motor[i].CUR_RPM);
+
+			if ((motor[i].PWM + output) >= MAX_PWM) {
+				motor[i].PWM = MAX_PWM;
+			} else if ((motor[i].PWM + output) <= MIN_PWM) {
+				motor[i].PWM = MIN_PWM;
+			} else {
+				motor[i].PWM += output;
+			}
+
+			if(motor[i].TAR_RPM == 0){
+				motor[i].PWM = 0;
+			}
+
+			__HAL_TIM_SET_COMPARE(&(motor[i].TIMER), motor[i].CHANNEL,
+					motor[i].PWM << LEFT_SHIFT);
+
+			sprintf(message, "Dir%d: %d Tar_RPM%d: %d Cur_RPM%d: %d PWM%d: %d\n\r", i, motor[i].DIR, i,
+					motor[i].TAR_RPM, i, motor[i].CUR_RPM, i, motor[i].PWM);
+			CDC_Transmit_FS((uint8_t*) message, strlen(message));
+			HAL_Delay(1);	//Doesn't work without this line
 		}
 
 		HAL_Delay(1);
@@ -411,7 +476,7 @@ static void MX_GPIO_Init(void) {
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12 | GPIO_PIN_14, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, MOT1_Dir_Pin | MOT2_Dir_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin : LED_Pin */
 	GPIO_InitStruct.Pin = LED_Pin;
@@ -420,24 +485,43 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
-	/*Configure GPIO pins : PB12 PB14 */
-	GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_14;
+	/*Configure GPIO pins : MOT1_Dir_Pin MOT2_Dir_Pin */
+	GPIO_InitStruct.Pin = MOT1_Dir_Pin | MOT2_Dir_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : MOT1_EXTIA_Pin MOT1_EXTIB_Pin MOT2_EXTIA_Pin MOT2_EXTIB_Pin */
+	GPIO_InitStruct.Pin = MOT1_EXTIA_Pin | MOT1_EXTIB_Pin | MOT2_EXTIA_Pin
+			| MOT2_EXTIB_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/* EXTI interrupt init*/
+	HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 	/* USER CODE BEGIN MX_GPIO_Init_2 */
 	/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
-{
-	if(errorFlag){
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	ms++;
+	if (errorFlag) {
 		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 	}
 
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == motor[0].A_PIN || GPIO_Pin == motor[0].B_PIN) {
+		updateEncoder(0);
+	} else if (GPIO_Pin == motor[1].A_PIN || GPIO_Pin == motor[1].B_PIN) {
+		updateEncoder(1);
+	}
 }
 
 /* USER CODE END 4 */
